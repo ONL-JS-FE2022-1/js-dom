4db78b54a8774409a9d78fdcf9f5ee43e964f6e3
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-21 20:47:39 +0200

    Hello, user

--- index.js
+++ index.js
@@ -4,10 +4,29 @@
 
 // console.dir(document);
 
-function getAlert(event) {
-    console.log(event);
-    event.target.removeEventListener('click', getAlert);
-}
+// function getAlert(event) {
+//     console.log(event);
+//     event.target.removeEventListener('click', getAlert);
+// }
 
-const buttonElement = document.getElementById('btn');
-buttonElement.addEventListener('click', getAlert);
+// const buttonElement = document.getElementById('btn');
+// buttonElement.addEventListener('click', (event) => {
+//     console.log(event);
+// });
+
+
+
+/*
+Напишіть функцію, яка вітає користувача за його ім'ям.
+Ім'я користувача брати з інпуту форми.
+*/
+
+const form = document.getElementById('hello-form');
+form.addEventListener('submit', helloUser)
+
+function helloUser(event) {
+    event.preventDefault();
+    const form = event.target;
+    const value = form[0].value;
+    console.log(`Hello, ${value ? value : 'Anonym'}`);
+}
