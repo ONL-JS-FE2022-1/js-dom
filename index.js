c7a7908654349f37b76872995439919f472b0129
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-22 18:46:13 +0200

    querySelector, querySelectorAll

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-const collection = document.getElementsByClassName('paragraph');
-console.log(collection);
+const paragraphCollection = document.querySelectorAll('p:nth-child(even)');
+console.log(paragraphCollection);
 
-for (const p of collection) {
-    p.style.color = 'green';
-}
-
-const btns = document.getElementsByTagName('button');
-console.log(btns);
-
-const arr = [...btns];
-console.log(arr);
-
-function hello() {
-    console.log('Hello, user!')
-}
-
-// variant 1
-// btns[0].addEventListener('click', hello);
-// btns[1].addEventListener('click', hello);
-// btns[2].addEventListener('click', hello);
-
-// variant 2
-for(const btn of btns) {
-    btn.addEventListener('click', hello);
+for(const par of paragraphCollection) {
+    par.style.color = 'red';
 }
