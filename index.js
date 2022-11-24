17d8603dc752976c09b3fe8b1fdb44f16690173e
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-24 18:52:32 +0200

    options and stopPropagation()

--- index.js
+++ index.js
@@ -2,13 +2,22 @@
 // console.log(event);
 
 const btn = document.querySelector('button');
+const div = document.querySelector('#root');
 
 const eventHandler = (event) => {
-    console.dir(event.currentTarget); // той, кому належить eventListener
-    console.dir(event.target); // той, на кому спрацювала подія
+    if(event.currentTarget === document.body) {
+        event.stopPropagation();
+        console.log('Ніяких тобі кнопок');
+    }
 }
-
-window.addEventListener('click', eventHandler);
+// console.group();
+btn.addEventListener('click', eventHandler, () => {
+    console.log('hello, button');
+});
+// div.addEventListener('click', eventHandler, {capture: true});
+document.body.addEventListener('click', eventHandler, {capture: true});
+// window.addEventListener('click', eventHandler, {capture: true});
+console.groupEnd();
 
 // const event = new MouseEvent('click');
 // btn.dispatchEvent(event);
