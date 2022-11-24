ef0f77ead507a5efff0e71c2eafc3356826106df
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-24 18:34:09 +0200

    Bubbling and capturing

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+// const event = new Event('click');
+// console.log(event);
+
+const btn = document.querySelector('button');
+
+const eventHandler = (event) => {
+    console.dir(event.currentTarget); // той, кому належить eventListener
+    console.dir(event.target); // той, на кому спрацювала подія
+}
+
+window.addEventListener('click', eventHandler);
+
+// const event = new MouseEvent('click');
+// btn.dispatchEvent(event);
+
 /*
-1. Маємо div
-2. Маємо дві кнопки - на одній написано "Зробити червоним", на другій - "Зробитии зеленим"
-3. За натисненням на кнопку фоновий колір оцього діва змінюється на відповідний колір, вказаний на кнопці.
+
+1. Фаза занурення. Подія стається на рівні OC. 
+OC передає її браузеру (Window), той передає document -> body -> .... -> елемент, на якому сталася подія
+
+2. Фаза цілі. Подія досягла елемента, елемент - це таргет
+
+3. Фаза сплиття. Подія починає спливати у зворотньому напрямку, тобто від елемента до браузера.
 
 */
-
-const div = document.querySelector('.box');
-const classList = div.classList;
-console.log(classList);
-const [red, green] = document.querySelectorAll('.btn');
-
-red.addEventListener('click', () => {
-    classList.toggle('red-little');
-})
-
-green.addEventListener('click', () => {
-    classList.toggle('green-big');
-})
