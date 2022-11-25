136f74819fe3ece276f42e9c56f9706fb47ca801
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-25 19:42:37 +0200

    Creating new html element

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-/*
-Дан section, на якій треба клацати мишею
-Дан div, який має переміститись на точку, в яку клацнули мишею
-*/
+// 1. Створити новий елемент
+const div = document.createElement('div');
+div.append('Hello, JS!');
+console.log(div);
+// 2. Приєднати елемент до будь-якого існуючого на сторінці батьківського елемента
+const body = document.body;
+console.log(body);
+body.append(div);
 
-const field = document.querySelector('#game-field');
-const box = document.querySelector('#box');
-const divCounter = document.querySelector('#counter');
 
-let counter = 0;
-
-field.addEventListener('click', clickHandler, {capture: true});
-
-function clickHandler(event) {
-    const {currentTarget, target, currentTarget:{children: {box}}} = event;
-    if(currentTarget !== target) {
-        updateCounter();
-    }
-
-    box.style.top = `${randomCoordinates(currentTarget.offsetHeight)}px`;
-    box.style.left = `${randomCoordinates(currentTarget.offsetWidth)}px`;
-}
-
-function updateCounter() {
-    divCounter.textContent = ++counter;
-}
-
-function randomCoordinates(max) {
-    return Math.floor(Math.random() * max);
-}
+//
+const div2 = document.createElement('div');
+div2.append('test');
+div.append(div2);
