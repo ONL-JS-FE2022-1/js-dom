d9f2306d63149b4d39d4337d6bcfa59144bb43f1
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-25 19:15:13 +0200

    Catch box game

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-function createDiv() {
-    const div = document.createElement('div'); // 1. Створюємо елемент у пам'яті
-    div.classList.add('box');
-    div.append('Hello text');
-    document.body.append(div); // 2. Приєднуємо елемент до батьківського елементу
+/*
+Дан section, на якій треба клацати мишею
+Дан div, який має переміститись на точку, в яку клацнули мишею
+*/
+
+const field = document.querySelector('#game-field');
+const box = document.querySelector('#box');
+const divCounter = document.querySelector('#counter');
+
+let counter = 0;
+
+field.addEventListener('click', clickHandler, {capture: true});
+
+function clickHandler(event) {
+    const {currentTarget, target, currentTarget:{children: {box}}} = event;
+    if(currentTarget !== target) {
+        updateCounter();
+    }
+
+    box.style.top = `${randomCoordinates(currentTarget.offsetHeight)}px`;
+    box.style.left = `${randomCoordinates(currentTarget.offsetWidth)}px`;
 }
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', createDiv);
+function updateCounter() {
+    divCounter.textContent = ++counter;
+}
+
+function randomCoordinates(max) {
+    return Math.floor(Math.random() * max);
+}
