66f58f63549d9f59b9ac45cdafcbfed3f6999ecc
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-25 20:53:23 +0200

    Create user card (need refactor)

--- index.js
+++ index.js
@@ -1,10 +1,90 @@
-/*
-Створити картинку і вставити її в div
-*/
+const telephone = {
+  name: "Samsung",
+  avatar: "./",
+  description: "BlaBlaBla",
+  price: "100$",
+};
 
-const div = document.createElement('div');
-const img = document.createElement('img');
-img.setAttribute('src', './images/7f041fc3ecd15391c19bf91c2c16ba32.jpg');
-div.append(img);
+const telephoneArray = [
+  {
+    name: "Samsung",
+    avatar: "./",
+    description: "BlaBlaBla",
+    price: "100$",
+  },
+  {
+    name: "Nokia",
+    avatar: "./",
+    description: "BlaBlaBla",
+    price: "50$",
+  },
+  {
+    name: "Huawei",
+    avatar: "./",
+    description: "BlaBlaBla",
+    price: "200$",
+  },
+  {
+    name: "iPhone",
+    avatar: "./",
+    description: "BlaBlaBla",
+    price: "100500$",
+  },
+];
 
-document.body.append(div);
+// console.log(`Телефон ${telephone.name} коштує ${telephone.price}`);
+
+// for(const tel of telephoneArray) {
+//     console.log(`Телефон ${tel.name} коштує ${tel.price}`);
+// }
+
+
+// СТРУКТУРА
+
+
+/* <article class="card-wrapper">
+    <img
+        class="card-image"
+        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+        alt="John"
+    />
+    <h2 class="username">John</h2>
+    <p class="description">fafewqrewrqwer2322revzd</p>
+</article> */
+
+
+// СТРУКТУРА
+
+const root = document.querySelector('#root');
+
+function createCard(user) {
+    /* TODO:
+    1. Refactor to short code
+    2. Create image placeholder
+    3. When image is loadeng, show placeholder
+    */
+    const article = document.createElement('article');
+    article.classList.add('card-wrapper');
+
+    const img = document.createElement('img');
+    img.classList.add('card-image');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('username');
+    h2.append(user.name);
+
+    const p = document.createElement('p');
+    p.classList.add('description');
+    p.append(user.description);
+
+    article.append(img, h2, p);
+    
+    return article;
+}
+
+const cardArray = data.map((user) => createCard(user));
+
+
+root.append(...cardArray);
