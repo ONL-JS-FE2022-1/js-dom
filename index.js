aab1c6a381b286a498e504587db64e3a9719be36
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-28 19:15:07 +0200

    Lazyload

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ const root = document.querySelector('#root');
 function createCard(user) {
     /* TODO:
     1+. Refactor to short code
-    2. Create image placeholder
+    2+. Create image placeholder
     3. When image is loadeng, show placeholder
     */
 
-    const img = document.createElement('img');
-    img.classList.add('card-image');
-    img.setAttribute('src', user.profilePicture);
-    img.setAttribute('alt', user.name);
+    const imageWrapper = createImageWrapper(user);
 
     const h2 = createElement('h2', {classNames: ['username']}, user.name);
 
     const p = createElement('p', {classNames: ['description']}, user.description);
     
-    return createElement('article', {classNames: ['card-wrapper']}, img, h2, p);
+    return createElement('article', {classNames: ['card-wrapper']}, imageWrapper, h2, p);
 }
 
 const cardArray = data.map((user) => createCard(user));
@@ -37,4 +34,35 @@ function createElement(type, {classNames}, ...children) {
   elem.classList.add(...classNames);
   elem.append(...children);
   return elem;
+}
+
+function createImageWrapper(user) {
+  const imgWrapper = createElement('div', {classNames: ['image-wrapper']});
+  imgWrapper.setAttribute('id', `wrapper${user.id}`);
+  const img = createUserImage(user);
+  return imgWrapper;
+}
+
+function createUserImage(user) {
+  const img = document.createElement('img');
+  img.classList.add('card-image');
+  img.setAttribute('src', user.profilePicture);
+  img.setAttribute('alt', user.name);
+  img.dataset.id = user.id;
+  
+  img.addEventListener('load', imageLoadHandler);
+  img.addEventListener('error', imageErrorHandler);
+
+  return img;
+}
+
+function imageLoadHandler({target}) {
+  console.log('image successfully loaded');
+  const parrentWrapper = document.getElementById(`wrapper${target.dataset.id}`)
+  parrentWrapper.append(target);
+}
+
+function imageErrorHandler({target}) {
+  target.remove();
+  console.log('image loading has error');
 }
