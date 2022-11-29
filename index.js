df75846228ddb174a6171272e6071061d0d6c508
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-29 20:52:35 +0200

    RegExp

--- index.js
+++ index.js
@@ -1,54 +1,15 @@
-const form = document.getElementById('form');
-const root = document.getElementById('root');
+const regexp = /^Test$/; // шукаємо слово "Тест"
+/^[A-Za-z]{2,5}$/; // шукаємо слово з 2-5 літер (великих чи маленьких)
+/^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$/ // email
 
-const state = []; // здесь будут таски
+const str1 = 'hello';
+const str2 = new String('hello');
 
-form.addEventListener('submit', addItem);
+// RegExp
 
-function addItem(event) {
-    event.preventDefault();
-    const {target, target: {textInput: {value}}} = event;
-    state.push(value);
-    const li = createItem(value);
-    root.append(li);
-    target.reset();
-}
+const testString = 'sunnyday23@gmail.com';
 
-function createItem(value) {
-    const li = document.createElement('li');
-    li.append(value);
-    li.classList.add('item');
-    return li;
-}
-
-
-
-/*
-
-+1. В html створити квадрат 50х50
-+2. За натисненням на квадрат створити інпут для вводу тексту
-+3. За ентером інпут зникає, а в квадраті відображається те, що було введено в інпут
-
-*/
-
-const box = document.querySelector('#box');
-
-box.addEventListener('click', boxClickHandler);
-
-function boxClickHandler(event) {
-    const input = document.createElement('input');
-    input.addEventListener('keydown', inputKeyHandler);
-    const parent = event.target.parentNode;
-    parent.append(input);
-    box.removeEventListener('click', boxClickHandler);
-}
-
-function inputKeyHandler({target, target: {value, previousElementSibling}}) {
-    if(event.keyCode === 13) {
-        // 2. Передали дані сусідньому елементу (боксу)
-        previousElementSibling.textContent = value;
-        // 1. знищили сам інпут
-        target.remove();
-        box.addEventListener('click', boxClickHandler);
-    }
-}
+const reg1 = /^[A-Za-z]{2,5}$/;
+console.log(reg1)
+const reg2 = new RegExp('^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$');
+console.log(reg2);
