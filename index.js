787db705df5d23f0b253a82cc77d9af5fbc02284
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-29 19:15:10 +0200

    div + input task resolve

--- index.js
+++ index.js
@@ -20,3 +20,35 @@ function createItem(value) {
     li.classList.add('item');
     return li;
 }
+
+
+
+/*
+
++1. В html створити квадрат 50х50
++2. За натисненням на квадрат створити інпут для вводу тексту
++3. За ентером інпут зникає, а в квадраті відображається те, що було введено в інпут
+
+*/
+
+const box = document.querySelector('#box');
+
+box.addEventListener('click', boxClickHandler);
+
+function boxClickHandler(event) {
+    const input = document.createElement('input');
+    input.addEventListener('keydown', inputKeyHandler);
+    const parent = event.target.parentNode;
+    parent.append(input);
+    box.removeEventListener('click', boxClickHandler);
+}
+
+function inputKeyHandler({target, target: {value, previousElementSibling}}) {
+    if(event.keyCode === 13) {
+        // 2. Передали дані сусідньому елементу (боксу)
+        previousElementSibling.textContent = value;
+        // 1. знищили сам інпут
+        target.remove();
+        box.addEventListener('click', boxClickHandler);
+    }
+}
