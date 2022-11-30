54f607f645a32bf1311653cfaf147cb54119da71
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-30 21:08:54 +0200

    JSON welcome

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const intervalID = setInterval(function () {console.log('LOGGING')}, 2000);
-// 3 <== id interval
-// 30 LOGGING
-// clearInterval(intervalID)
+/* JSON - JavaScript Object Notation */
 
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    avatar: 'https://.....',
+    age: 18,
+    footSize: 46,
+    address: {
+        street: 'Testovaya',
+        house: 32
+    },
+    key: null,
+    sayHello: function() { // function will be ignored
+        console.log(`${this.firstName} say: Hello!`);
+    }
+}
