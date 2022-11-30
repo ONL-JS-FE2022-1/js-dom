1073f529c0ab0210035efcd86fa57fa6a32b6646
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-11-30 19:54:47 +0200

    setTimeout()

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const regexp = /^Test$/; // шукаємо слово "Тест"
-/^[A-Za-z]{2,5}$/; // шукаємо слово з 2-5 літер (великих чи маленьких)
-/^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$/ // email
+setTimeout(func, 2000);
 
-const str1 = 'hello';
-const str2 = new String('hello');
+function func() {
+    console.log(2+2);
+}
 
-// RegExp
+const helloTimeoutId = setTimeout(function () {console.log('LOGGING')}, 15000);
+clearTimeout(helloTimeoutId);
 
-const testString = 'sunnyday23@gmail.com';
-
-const reg1 = /^[A-Za-z]{2,5}$/;
-console.log(reg1)
-const reg2 = new RegExp('^[A-Za-z0-9\_\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+$');
-console.log(reg2);
+// func();
