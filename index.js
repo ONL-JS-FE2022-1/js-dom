a148e6e96df9fde9ec0598399889ef77f66f236c
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-01 20:53:58 +0200

    Creating Promise

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const p = fetch("./user.json")
-  .then((response) => {
-    // 0 && 1 (JSON)
-    return response.json();
-  })
-  .then((data) => {
-    // JS OBJECT
+const p = new Promise(executor);
+
+console.log(p);
+
+function executor(resolve, reject) {
+  const number = Math.floor(Math.random() * 15); // генерація цілого випадкового числа вдіапазоні від 0 до 15
+  if (number % 2 === 0) {
+    // якщо це число кратне 2
+    resolve(number);
+  } else {
+    // якщо число не кратне 2
+    const err = new RangeError("ERROR HAPPENED!");
+    reject(err);
+  }
+}
+
+p.then(
+  (data) => {
     console.log(data);
-  })
-  .catch((err) => {
-    console.log("ERROR HAVE HAPPENNED: " + err);
-  })
-  .finally(() => {
-    console.log("FINALLY question closed");
-  });
+  },
+  (err) => {
+    console.log(err);
+  }
+);
