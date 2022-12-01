2e2c495ae949441ce42c50934716a2e4ea4f5e6a
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-01 20:11:40 +0200

    Then. Catch, finally

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const p = fetch('./user.json')
-  .then((response) => { // 0 && 1 (JSON)
+const p = fetch("./user.json")
+  .then((response) => {
+    // 0 && 1 (JSON)
     return response.json();
   })
-  .then((data) => { // JS OBJECT
+  .then((data) => {
+    // JS OBJECT
     console.log(data);
+  })
+  .catch((err) => {
+    console.log("ERROR HAVE HAPPENNED: " + err);
+  })
+  .finally(() => {
+    console.log("FINALLY question closed");
   });
