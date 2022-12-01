ddb0a836c948fbca0cc833e1879cd1d8d52564ca
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-01 19:49:50 +0200

    promise , fetch welcome

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-/* JSON - JavaScript Object Notation */
-
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    avatar: 'https://.....',
-    age: 18,
-    footSize: 46,
-    address: {
-        street: 'Testovaya',
-        house: 32
-    },
-    key: null,
-    sayHello: function() { // function will be ignored
-        console.log(`${this.firstName} say: Hello!`);
-    }
-}
+const p = fetch('./user.json')
+  .then((response) => { // 0 && 1 (JSON)
+    return response.json();
+  })
+  .then((data) => { // JS OBJECT
+    console.log(data);
+  });
