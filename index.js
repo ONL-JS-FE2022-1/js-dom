11c9e1ab6871873ec8cba1b61229add3739bf94e
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-02 18:18:38 +0200

    Promise

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const p = new Promise(executor);
-
-console.log(p);
-
-function executor(resolve, reject) {
-  const number = Math.floor(Math.random() * 15); // генерація цілого випадкового числа вдіапазоні від 0 до 15
-  if (number % 2 === 0) {
-    // якщо це число кратне 2
-    resolve(number);
-  } else {
-    // якщо число не кратне 2
-    const err = new RangeError("ERROR HAPPENED!");
-    reject(err);
-  }
-}
+const p = new Promise(function(resolve, reject) { // status: pending
+  setTimeout(() => {
+    resolve('It time'); // время пришло!!!
+  }, 15000)
+})
 
 p.then(
   (data) => {
