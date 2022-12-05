4b4645d595c599813bb3dc13f69070c089b149b1
Roman Ratushnyi <freshjs.ofe@gmail.com>
2022-12-05 20:18:40 +0200

    Async/await

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-fetch('https://api.monobank.ua/bank/currency')
-.then((response) => response.json())
-.then((data) => {replaceCurrencyCode(data)})
+// async await
 
-function replaceCurrencyCode(courseData) {
-  fetch('https://pkgstore.datahub.io/core/currency-codes/codes-all_json/data/029be9faf6547aba93d64384f7444774/codes-all_json.json')
-  .then((response) => response.json())
-  .then((data) => {
-    const mapWithCodes = new Map();
+const url = 'https://randomuser.me/api/';
 
-    data.map((item) => {mapWithCodes.set(item.NumericCode, item.Currency)})
-
-    courseData.forEach((currentCurrency) => {
-      currentCurrency.currencyCodeA = mapWithCodes.get(currentCurrency.currencyCodeA);
-      currentCurrency.currencyCodeB = mapWithCodes.get(currentCurrency.currencyCodeB);
-    })
-
-    showCourse(courseData);
-  })
-}
-
-function showCourse(courseData) {
-  courseData.map((item) => {
-    if(item.rateBuy && item.rateSell) {
-      const div = document.createElement('div');
-      div.append(`
-      Монобанк продает ${item.currencyCodeA} за ${item.rateSell} ${item.currencyCodeB}, а покупает за ${item.rateBuy} ${item.currencyCodeB}
-      `);
-  
-      document.body.append(div);
-    }
-  })
+async function loadData(url) {
+  const p = fetch(url);
+  const res = await p;
+  return res;
 }
